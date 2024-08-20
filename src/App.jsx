eb573a64bd20d18b949bae129b6913f2c53bf6d8
Siddharth Chauhan <sidchauhan583@gmail.com>
2024-08-20 16:20:40 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavHero from './components/NavHero'
+import Movies from './components/Movies'
+import Questions from './components/Questions'
+
+function App() {
+  return (
+    <div>
+      <NavHero />
+      <Movies/>
+      <Questions />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/NavHero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NavHero = () => {
+  return (
+    <div>NavHero</div>
+  )
+}
+
+export default NavHero
